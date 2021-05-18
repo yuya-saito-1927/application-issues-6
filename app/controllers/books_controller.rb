2b46625d948ab988c2757b242c9261a65a765699
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-05-18 13:11:01 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,23 +1,34 @@
 class BooksController < ApplicationController
-  
+
   def new
     @book = Book.new
-  end 
-  
+  end
+
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to book_path(@book.id)
-  end 
-  
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
+
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+
    def show
-    @user = User.find(params[:id])
-   end 
-    
+    @book = Book.find(params[:id])
+   end
+
     private
-    
+
     def book_params
-      params.require(:book).permit(:title, :opinion, :user_id)
-    end 
-  
+      params.require(:book).permit(:title, :opinion)
+    end
+
 end

4dec8b7f288ef33703a1c50ff42cfd4126b4124a
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-05-20 11:11:39 +0000

    Fix

--- db/migrate/20210516065457_create_books.rb
+++ db/migrate/20210516065457_create_books.rb
@@ -2,7 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.text :body
+      t.text :opinion
       t.integer :user_id
 
       t.timestamps

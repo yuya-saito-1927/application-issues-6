9649b92a20d3e7fa04c984f5192b41efe5b72316
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-06-05 15:56:39 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  # フォローしていれば、trueを返す
+  def following?(user)
+    followings.include?(user)
+  end
+
 
   validates :name, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to:'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' =>'relationships#followers', as: 'followers'
+  end 
   resources :books do
     resources :post_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  post 'follow/:id' => 'relationship#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationship#unfollow', as: 'unfollow'
 end

1829a180557e693928e47b3561c7f8d8213a5701
yuya-saito-1927 <sanda.yokoyama.nanpei@gmail.com>
2021-06-06 07:12:28 +0000

    finish

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,56 @@
+class SearchController < ApplicationController
+  
+  def search
+    @model = params["search"]["model"]
+    @value = params["search"]["value"]
+    @how = params["search"]["how"]
+    @dates = search_for(@how, @model, @value)
+  end 
+  
+  private
+  
+  def match(model, value)
+    if model == 'user'
+      User.where(name: value)
+    elsif model == 'book'
+      Book.where(title: value)
+    end 
+  end 
+  
+  def forward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{value}%")
+    end 
+  end 
+  
+  def backward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}")
+    end 
+  end 
+  
+  def partical(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}%")
+    end 
+  end 
+  
+  def search_for(how, model, value)
+    case how
+    when 'match'
+      match(model, value)
+    when 'forward'
+      forward(model, value)
+    when 'backward'
+      backward(model, value)
+    when 'partical'
+      partical(model, value)
+    end 
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/search' => 'search#search'
   get 'favorites/create'
   get 'favorites/destroy'
   devise_for :users
